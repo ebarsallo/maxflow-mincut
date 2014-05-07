99e0dd5c23c92d8f5123ca0fe43ae9a26ee37233
Edgardo Alberto Barsallo Yi <ebarsallo@mint>
2014-05-07 13:08:04 -0400

    Removed decidability of equality lemma, added test to list fixpoints (In).

--- GraphExample.v
+++ GraphExample.v
@@ -3,12 +3,15 @@ Require Export Graph.
 (* ###################################################### *)
 (** * Definitions *)
 (** Vertices Name [VerName] *)
+Definition V5 := index 5.  (* not in graph *)
 Definition V4 := index 4.
 Definition V3 := index 3.
 Definition V2 := index 2.
 Definition V1 := index 1.
 
+
 (** Vertices *)
+Definition vtx5 := v_cons V5 nil.
 Definition vtx4 := v_cons V4 nil.
 Definition vtx2 := v_cons V2 (cons (pair (pair V4 5) 5) nil ).
 Definition vtx3 := v_cons V3 (cons (pair (pair V4 4) 4) 
@@ -27,7 +30,7 @@ Definition vtx1 := v_cons V1 (cons (pair (pair V2 10) 4)
 Definition G := pair (pair (cons vtx1 (cons vtx2 (cons vtx3 (cons vtx4 nil)))) 
                           V1) V4.
 
-
+(* G' is not well-formed since has duplicated vertices*)
 Definition G' := pair (pair (cons vtx1 (cons vtx2 (cons vtx3 (cons vtx4 (cons vtx1 nil))))) 
                           V1) V4.
 
@@ -84,116 +87,36 @@ Example test_capacity_constraint_1 :
   ble_weight (edge_flow G V1 V2) (edge_weight G V1 V2) = true.
 Proof. reflexivity. Qed.
 
-(** 
-Definition True : Prop := forall (P:Prop), P -> P.
+(* Test: List functions *)
+Example test_in1 :
+  In vtx5 (graph_list G) beq_vertex = false.
+Proof. reflexivity. Qed.
 
-Theorem true_theorem:
-        True.
-Proof.
-        unfold True.
-        intros P H.
-        apply H.
-        Qed.
-*)
-(*
-Definition beq_vertex' (v1 v2: vertex) : Prop :=
-  match v1 with
-    | v_cons vn1 vlist1 => 
-      match v2 with
-        | v_cons vn2 vlist2 => 
-           if beq_VerName vn1 vn2 then True else False
-      end 
-  end.
+Example test_in2 :
+  In vtx4 (graph_list G) beq_vertex = true.
+Proof. reflexivity. Qed.
 
-Check beq_nat_refl.
-
-Lemma beq_VerName_refl :
-  forall (v :VerName), beq_VerName v v = true.
-Proof.
-  intros.
-  destruct v.
-  simpl. rewrite <- beq_nat_refl.
-  reflexivity.
-Qed.
+Example test_in3 :
+  In vtx1 (graph_list G') beq_vertex = true.
+Proof. reflexivity. Qed.
 
 
-Lemma test :
-  forall (v1 v2 :vertex), v1 = v2 -> beq_vertex' v1 v2.
-Proof.
-  intros.
-  unfold beq_vertex'.
-  destruct v1.
-  destruct v2. inversion H. simpl.
-  rewrite beq_VerName_refl.
-  admit.
-Qed.
-*)
-
-Lemma eq_VerName_dec : 
-  forall vn1 vn2 :VerName, {vn1 = vn2} + {vn1 <> vn2}.
-Proof.
-  intros vn1 vn2.
-  destruct vn1 as [n1]. destruct vn2 as [n2].
-  destruct (eq_nat_dec n1 n2) as [Heq | Hneq].
-  Case "n1 = n2".
-    left. rewrite Heq. reflexivity.
-  Case "n1 <> n2".
-    right. intros contra. inversion contra. apply Hneq. apply H0.
-Qed.
-
-Lemma eq_adjacence_dec : 
-  forall vls1 vls2 :adjacence, {vls1 = vls2} + {vls1 <> vls2}.
-Proof.
-  intros vls1 vls2.
-  destruct vls1 as [p1 f1]. destruct vls2 as [p2 f2].
-  destruct p1 as [vn1 w1].  destruct p2 as [vn2 w2].
-  destruct (eq_VerName_dec vn1 vn2) as [Heq | Hneq].
-    destruct (eq_nat_dec w1 w2) as [HWeq | HWneq];
-    destruct (eq_nat_dec f1 f2) as [HFeq | HFneq];
-      try (left; subst; reflexivity);
-      try (right; intros contra; inversion contra; auto).
-  right. intros contra. inversion contra. auto.
-Qed.
-
-Lemma eq_ladj_dec : 
-  forall ls1 ls2 :list adjacence, {ls1 = ls2} + {ls1 <> ls2}.
-Proof.
-  intros ls1 ls2.
-  apply list_eq_dec.
-  apply eq_adjacence_dec.
-Qed.
-
-Lemma eq_vertex_dec : 
-  forall v1 v2 :vertex, {v1 = v2} + {v1 <> v2}.
-Proof.
-  intros v1 v2.
-  destruct v1 as [vn1 ls1]. destruct v2 as [vn2 ls2].
-  destruct (eq_VerName_dec vn1 vn2) as [Heq | Hneq].
-  destruct (eq_ladj_dec ls1 ls2) as [Hleq | Hlneq];
-      try (left; subst; reflexivity);
-      try (right; intros contra; inversion contra; auto).
-  try (right; intros contra; inversion contra; auto).
-Qed.
-
-
-Print NoDup.
-Print NoDup_remove_1.
-SearchAbout "NoDup".
-
-Lemma nodup_listv :
-  forall l :list vertex, NoRep (l).
+Lemma norepeat_listv :
+  forall l :list vertex, no_repeat l.
 Proof.
   intros.
   induction l.
-  apply NoRep_nil.
-  apply NoRep_cons with (f:=beq_vertex). admit.
-  assumption.
+  Case "list is empty". apply nr_nil.
+Print nr_cons.
+  Case "list has elements".
+    apply nr_cons. assumption (* IHl *). 
+    unfold not. intros. admit.
 Qed.
 
 
-(* Test: is G well-formed? *)
+(* Test: is G well-formed? Yes *)
 Print G. 
-Theorem test_graph_well_formed :
+Theorem test_graph_well_formed_G :
   graph_well_formed G.
 Proof.
   unfold G.
@@ -201,5 +124,17 @@ Proof.
   simpl. reflexivity.
   apply list_eq_dec. apply eq_vertex_dec.
   simpl. reflexivity. 
-  apply nodup_listv.
+  simpl. 
+  apply norepeat_listv.
 Qed.
+
+(* Test: is G' well-formed? No *)
+Print G'. 
+Theorem test_graph_well_formed_G' :
+  ~ graph_well_formed G'.
+Proof.
+  unfold G', not.
+  intros. inversion H.
+  inversion H1. 
+  Abort.
+
